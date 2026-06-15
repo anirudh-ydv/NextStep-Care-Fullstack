aa8ca2f97e26188de281a9a36a1bd18704cc4881
Anirudh <yadavanirudha4169@gmail.com>
2026-06-15 22:41:57 +0530

    fix: updated nodemailer port to 587 for render

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,10 @@ const bcrypt = require('bcryptjs');
 // EMAIL ENGINE SETUP (The Postman)
 // ==========================================
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, 
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
